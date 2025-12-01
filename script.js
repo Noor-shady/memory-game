cd341528e05aa270d13ee23e01953476431843db
noor <c3074048@newcastle.ac.uk>
2025-12-01 13:41:56 +0000

    shuffle function and initialize Memory Match game setup

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ let matchesFound = 0;
 let timeLeft = 60; 
 let timerInterval;
 
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+// Initialize Game
+function initGame() {
+    const shuffledEmojis = shuffle(emojis);
